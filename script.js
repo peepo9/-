1dc7e77b54f03a8d2add43f17073336b6987f26f
peepo9 <patapee16112550@gmail.com>
2026-09-16 16:36:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
    ตรวจจับหน้าปัจจุบันจาก element ที่มีอยู่ในหน้านั้นๆ
    ========================================================= */
 
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwZYCnI4r30bpVfItYJziVbHyOY8CVUVYeEFFnvx2b4pid2-Ba2ANmuOLScHIj5EIUB/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwwuZ7KpdNz4k1d5BeItruPVe_S37ldWBfk11AU9f3HX3xHzMMXBfvyUJI7sbLfSFVB/exec';
 const SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQVX1ZbDi1j3LMCGVPGhNWbf_TXyO0tOHsy56RZJe07-rMc0vXgfzdQANELMwxyW1ygAKSFxCm80wtV/pub?gid=0&single=true&output=csv';
 const PRODUCTS_JSON_URL = 'products.json';
 
